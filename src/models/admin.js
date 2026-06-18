a955a3234631a5b1e30df265f2a1a91dc6d3816f
Neelam-Mahato <141303237+Neelam-Mahato@users.noreply.github.com>
2026-06-18 17:15:32 +0530

    Update admin.js

--- src/models/admin.js
+++ src/models/admin.js
@@ -100,23 +100,12 @@ const db = require("../config/db");
    const manageRegistration= async (payload) => {
     try{ 
       const param = [payload.token];
-      const query1 = `Select admin_token from admin `;
-      const [result1] = await db.execute(query1);
-      const query = `Select admin_token from admin where admin_token = ?`;
-      const [result] = await db.execute(query,param);
-       if(result1.length > 0 && result.length == 0 ){
-        return {success:false,msg:1} 
-      }
-      if(result[0].admin_token == null || result[0].admin_token == "" || result[0].admin_token == 'null'){
-        return {success:false ,msg:2} 
-      }
-      else 
-      {
-        const params = [payload.register_status, payload.uid,payload_token, payload.registerid];
-        const query = `Update registered_members set register_status = ? , uid = ?, qr_code = ? where registerid = ?`;
+      
+        const params = [payload.register_status, payload.uid,payload.qr_code, payload.register_id];
+        const query = `Update registered_members set register_status = ? , uid = ?, qr_code = ? where register_id = ?`;
         const [result1] = await db.execute(query,params);
-        return result11.affectedRows == 1 ? {success:true} : {success: false}; 
-      }
+        return result1.affectedRows == 1 ? {success:true} : {success: false}; 
+      
     
      } catch (error) {
     console.error(' Error:', error);
