d37bb5ba42ebcaaa3d5dcdefbb67ea3ca70db901
Neelam-Mahato <141303237+Neelam-Mahato@users.noreply.github.com>
2026-06-18 21:27:56 +0530

    Update index.js

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,4 +1,6 @@
- const nodemailer = require('nodemailer');
+ // const nodemailer = require('nodemailer');
+import nodemailer from 'nodemailer';
+
  const email = "nosheindia.2026@gmail.com";
  const pass = "pyso ehtv xpls ckmo";
 const port = 587;
