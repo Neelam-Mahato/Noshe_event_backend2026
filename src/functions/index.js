41dbb21a0b0848c7f8005df43221d46f9425b7c4
Neelam-Mahato <141303237+Neelam-Mahato@users.noreply.github.com>
2026-06-20 05:23:32 +0530

    Add IPv4 family option to email transporter

--- src/functions/index.js
+++ src/functions/index.js
@@ -4,6 +4,7 @@ const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: Number(process.env.EMAIL_PORT || 587),
   secure: Number(process.env.EMAIL_PORT || 587) === 465,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
