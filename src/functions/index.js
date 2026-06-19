15cca90c1638bab95809a8d9cc459f1e0ed16949
Neelam-Mahato <141303237+Neelam-Mahato@users.noreply.github.com>
2026-06-20 05:28:59 +0530

    Increase timeout values for connection and greeting

--- src/functions/index.js
+++ src/functions/index.js
@@ -9,9 +9,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  connectionTimeout: 30000,
-  greetingTimeout: 30000,
-  socketTimeout: 30000,
+  connectionTimeout: 50000,
+  greetingTimeout: 50000,
+  socketTimeout: 50000,
   tls: {
     rejectUnauthorized: false,
   },
