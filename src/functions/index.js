5a63057e89ded41e919ffcacd5cafbb618dc56f6
Neelam Mahato <neelammahato3@gmail.com>
2026-06-25 06:15:03 +0530

    mail changes

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,14 +1,14 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
+  host: process.env.EMAIL_HOST,
   port: Number(process.env.EMAIL_PORT || 587),
   secure: Number(process.env.EMAIL_PORT || 587) === 465,
   // family: 4,
   secure:false,
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_KEY,
+    pass: 'itat vikc jcua naal',
   },
   connectionTimeout: 100000,
   greetingTimeout: 50000,
