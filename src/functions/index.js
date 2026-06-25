af0a9d311c0042120bfeb46fac9d4849409678cf
Neelam Mahato <neelammahato3@gmail.com>
2026-06-25 09:56:02 +0530

    chamges done

--- src/functions/index.js
+++ src/functions/index.js
@@ -83,7 +83,7 @@ async function sendQrEmail(recipientEmail, username, qrCodeBase64) {
   `;
 
   await resend.emails.send({
-  from: "<onboarding@resend.dev>",
+  from: `"Your App Team " <onboarding@resend.dev>`,
   to: recipientEmail,
   subject: "Your Registration Security QR Code",
   html: html,
@@ -103,7 +103,7 @@ async function sendEmail(recipientEmail, username) {
   }
 
   await resend.emails.send({
-  from: "<onboarding@resend.dev>",
+  from: `"Your App Team " <onboarding@resend.dev>`,
    to: recipientEmail,
     subject: 'Request declined',
     html: `
